0435335f7fabe4cb7937d4aad5c5fcfd76ae4b8a
박홍철 <parkhc@baghongcheol-ui-MacBookAir.local>
2023-05-09 01:33:27 +0900

    3

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -7,3 +8,6 @@ app = FastAPI()
 async def root(body: dict):
     print(body)
     return "Done"
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='0.0.0.0', port=5010)
