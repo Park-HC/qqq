02b9fafd7e6a859175d168af785123360f3764b7
Park-HC <Park-HC@github.com>
2023-05-09 09:15:10 +0900

    dsafa

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 
-@app.post("/callback")
-async def root(body: dict):
+@app.post("/callback", response_class=HTMLResponse)
+async def callback(request: Request, body: dict):
     print(body)
-    return "Done"
+    return templates.TemplateResponse("item.html", {"request": request, "body": body})
+
+
+@app.get('/')
+def hello_world():
+    print("hello from hello_word")
+    return {'message': 'hello'}
+
+
+@app.get("/items/{id}", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse("item.html", {"request": request, "id": "adsfasdf"})
+
 
 if __name__ == "__main__":
-    uvicorn.run(app, host='0.0.0.0', port=5010)
+    uvicorn.run(app)
